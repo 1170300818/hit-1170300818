e4241ff86be697a409df056977da440f4a90b0b2
fantianxiang <47970447+1170300815@users.noreply.github.com>
2019-07-07 18:10:49 +0800

    Change the variable name
    Change the variable name

--- 123456/src/dadaw/JustForUse.java
+++ 123456/src/dadaw/JustForUse.java
@@ -6,11 +6,10 @@ import java.util.List;
 
 public class JustForUse {
   public static void main(String[] args) {
-
-    List<Integer> asd = new LinkedList<>();
-    asd.add(3);
-    asd.add(10);
-    Iterator<Integer> iter = asd.iterator();
+    List<Integer> temp = new LinkedList<>();
+    temp.add(3);
+    temp.add(10);
+    Iterator<Integer> iter = temp.iterator();
     while (iter.hasNext()) {
       Integer line = iter.next();
       if (line.equals(3)) {
@@ -18,5 +17,4 @@ public class JustForUse {
       }
     }
   }
-
 }
